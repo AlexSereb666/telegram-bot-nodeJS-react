5709de7a57b859795d3d4803d4722d5a7e19320b
Alexey <Alexey@mail.ru>
2024-02-07 15:06:10 +0300

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
 
 function App() {
   const {tg, user, onClose, onToggleButton} = useTelegram();
+  const [userData, setUserData] = useState(user); // Используйте состояние для отслеживания данных пользователя
 
   useEffect(() => {
     tg.ready();
-    console.log(user)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  // Обновляем данные пользователя, когда они изменяются
+  useEffect(() => {
+    setUserData(user);
+  }, [user]);
 
   return (
     <div className="App">
       work<br/>
       <button onClick={onClose}>Закрыть</button><br/>
-      <div>name: {user?.username}</div><br/>
+      <div>name: {userData?.username}</div><br/>
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
